1454332c43660357bb244e76cbe7ebbd74276103
ksliu25 <ksliu25@gmail.com>
2015-04-22 23:02:49 -0500

    Fixed authenticate syntax error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with_name: "admin", password: "admin",
+  http_basic_authenticate_with name: "admin", password: "admin",
   except: [:index, :show]
   def index
     @articles = Article.all
